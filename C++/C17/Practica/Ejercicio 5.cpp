d3e3c88d6f44b99da9a83aff9d213fbbce11e244
Ana Belén Salmi <50645823+aweeniii@users.noreply.github.com>
2019-05-30 08:52:35 -0300

    Actualizo ejercicio 5
    Faltaba el final equis de

--- C++/C17/Practica/Ejercicio 5.cpp
+++ C++/C17/Practica/Ejercicio 5.cpp
@@ -11,3 +11,10 @@ int main (){
 		cout<<"Introduzca un valor: ";
 		cin>>VEC[i];
 	}
+	i=0;
+	for (i=0; i<6; i++){
+		if (VEC[i]==VEC[i-1] or VEC[i]==VEC[i+1])
+			cout<<"El valor en VEC["<<i<<"] se repite"<<endl;
+	}
+	return (0);
+}
