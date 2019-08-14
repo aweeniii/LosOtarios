5568f411955b588e3c9e022998f7d112d901ddb6
pineapplepicsa <pineapplepizza@vivaldi.net>
2019-08-14 08:00:07 -0300

    creado, sin terminar

---
+++ C++/C25/Ejercicio 2.cpp
@@ -0,0 +1,13 @@
+/*Ejercicio 2.
+Escribir un programa en lenguaje C que permita cargar un vector de N elementos
+de tipo float, luego ingrese un número e informe si ese número está presente
+en el vector y cuántas veces aparece.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  
+}
